4767fe64549e7e343916b341f0b7c7c8e8c2229d
bilal iken <bilal11iken@gmail.com>
2026-05-05 15:17:55 +0100

    initialisation du projet

---
+++ src/test/java/com/example/gestiondestaches/GestionDestachesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestiondestaches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDestachesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
