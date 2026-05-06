6218ec58f70915c1576c27c1cbfb6a66e436b380
bilal iken <bilal11iken@gmail.com>
2026-05-06 01:17:26 +0100

    add login requist

---
+++ src/main/java/com/example/gestiondestaches/dto/LoginRequest.java
@@ -0,0 +1,21 @@
+package com.example.gestiondestaches.dto;
+
+
+import  jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+@Data
+
+public class LoginRequest {
+
+    @NotBlank
+    @Email
+    private  String email ;
+
+    @NotBlank
+    private  String password;
+    
+
+
+}
